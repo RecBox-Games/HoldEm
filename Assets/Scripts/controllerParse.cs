75f6e4908d1c3605cedc19b2222f166c71eab9e1
DigitalSRC <60196645+DigitalSRC@users.noreply.github.com>
2023-09-08 20:56:00 -0700

    Update controllerParse.cs
    Remove the packageMangaer as it was failing to compile with the unity builds.

--- Assets/Scripts/controllerParse.cs
+++ Assets/Scripts/controllerParse.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
-using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.UI;
 
